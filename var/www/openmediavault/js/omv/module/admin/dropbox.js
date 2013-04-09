119141fe82d0f4fadb112a19713afe4bda8b2ba6
Lorenzo Dieryckx <lorenzo.dieryckx+github@gmail.com>
2013-04-09 13:23:46 +0200

    Finished GUI panel & started impl of back-end rpc service

--- var/www/openmediavault/js/omv/module/admin/dropbox.js
+++ var/www/openmediavault/js/omv/module/admin/dropbox.js
@@ -14,14 +14,10 @@ OMV.NavigationPanelMgr.registerMenu("services", "dropbox", {
 OMV.Module.Services.dropbox = function (config) {
 	var initialConfig = {
 		rpcService  :"dropbox",
-		layout      :"vbox",
-		layoutConfig:{
-			align:"stretch"
-		}
 	};
 	Ext.apply(initialConfig, config);
 	OMV.Module.Services.dropbox.superclass.constructor.call(this, initialConfig);
-
+};
 
 Ext.extend(OMV.Module.Services.dropbox, OMV.FormPanelExt, {
 
@@ -41,7 +37,8 @@ Ext.extend(OMV.Module.Services.dropbox, OMV.FormPanelExt, {
 	},
 
 	getFormItems:function () {
-		return [
+		return 
+		[
 			{
 				xtype   :"fieldset",
 				title   :_("General settings"),
@@ -57,147 +54,22 @@ Ext.extend(OMV.Module.Services.dropbox, OMV.FormPanelExt, {
 						inputValue:1
 					},
 					{
-						xtype     :"textfield",
-						name      :"name",
-						width     :300,
-						value     :_("MiniDLNA on OpenMediaVault"),
-						fieldLabel:_("Name"),
-						plugins   :[ OMV.form.plugins.FieldInfo ],
-						infoText  :_("The name that is displayed to DLNA clients.")
-					},
-					{
-						xtype        :"numberfield",
-						name         :"port",
-						fieldLabel   :_("Port"),
-						vtype        :"port",
-						minValue     :0,
-						maxValue     :65535,
-						allowDecimals:false,
-						allowNegative:false,
-						allowBlank   :false,
-						value        :8200,
-						plugins      :[ OMV.form.plugins.FieldInfo ],
-						infoText     :_("Port to listen on.")
-					},
-					{
-						xtype     :"checkbox",
-						name      :"strict",
-						fieldLabel:_("Strict DLNA"),
-						boxLabel  :_("Strictly adhere to DLNA standards"),
-						checked   :false,
-						inputValue:1,
-						plugins   :[ OMV.form.plugins.FieldInfo ],
-						infoText  :_("This will allow server-side downscaling of very large JPEG images")
-					},
-					{
-						xtype     :"checkbox",
-						name      :"tivo",
-						fieldLabel:_("TiVo support"),
-						checked   :false,
-						inputValue:1
-					},
-					{
-						xtype     :"checkbox",
-						name      :"rescan",
-						fieldLabel:_("Media Rescan"),
-						checked   :false,
-						inputValue:1,
-						boxLabel  :_("Force a complete rescan of all media folders")
-					},
-					{
-						xtype:"label",
-						html :_("A complete rescan should not normally be needed. When files / folders are updated (added/deleted/modified), miniDLNA will automatically rescan updated items.") + "<br /><br />"
-					}
-				]
-			},
-			{
-				xtype       :"panel",
-				frame       :true,
-				title       :_("Media Folders (Media folders and files must be readable by the 'minidlna' user)"),
-				layout      :'fit',
-				boxMinHieght:200,
-				flex        :1,
-				items       :[
-					{
-						xtype     :"grid",
-						id        :'minidlna-shares-grid',
-						tbar      :[
-							{
-								id     :this.getId() + "-add",
-								xtype  :"button",
-								text   :_("Add"),
-								icon   :"images/add.png",
-								handler:function () {
-									var wnd = new OMV.Module.Services.MiniDLNAFolderPropertyDialog({uuid:OMV.UUID_UNDEFINED});
-									wnd.show();
-								}
-							},
-							{
-								id     :this.getId() + "-edit",
-								xtype  :"button",
-								text   :_("Edit"),
-								icon   :"images/edit.png",
-								handler:function (b) {
-									var record = b.ownerCt.ownerCt.getSelectionModel().getSelected();
-									var wnd = new OMV.Module.Services.MiniDLNAFolderPropertyDialog({
-										uuid :'dummy',
-										sfref:record.get('sfref'),
-										mtype:record.get('mtype')
-									});
-									wnd.show();
-								}
-							},
-							{
-								id     :this.getId() + "-remove",
-								xtype  :"button",
-								text   :_("Remove"),
-								icon   :"images/delete.png",
-								handler:function (b) {
-									b.ownerCt.ownerCt.getStore().remove(b.ownerCt.ownerCt.getSelectionModel().getSelected());
-								}
-							}
-						],
-						viewConfig:{ forceFit:true },
-						sm        :new Ext.grid.RowSelectionModel({
-							singleSelect:true
-						}),
-						store     :new Ext.data.SimpleStore({
-							fields:[
-								{ name:"sfref" },
-								{ name:"name" },
-								{ name:"mtype" }
-							],
-							data  :[ ]
-						}),
-						columns   :[
-							{
-								header   :_("Share"),
-								dataIndex:"name"
-							},
-							{
-								header   :"Content Type(s)",
-								dataIndex:"mtype",
-								renderer :function (val) {
-
-									switch (val) {
-										case 'A':
-											return _('Audio');
-										case 'V':
-											return _('Video');
-										case 'P':
-											return _('Images');
-									}
-									return _('All');
-								}
-							}
-						]
+						xtype: "sharedfoldercombo",
+						name: "sharedfolderref",
+						hiddenName: "sharedfolderref",
+						fieldLabel: _("Shared folder"),
+						allowNone: false,
+						plugins: [ OMV.form.plugins.FieldInfo ],
+						infoText: _("The location into which dropbox will synchronize")
 					}
 				]
 			}
-		];
+		]
 	}
 
 });
-OMV.NavigationPanelMgr.registerPanel("services", "minidlna", {
-	cls:OMV.Module.Services.MiniDLNA
+
+
+OMV.NavigationPanelMgr.registerPanel("services", "dropbox", {
+	cls:OMV.Module.Services.dropbox
 });
